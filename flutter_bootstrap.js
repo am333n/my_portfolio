87b9e958f5deec7b14188e21f0fcf597a1f519e9
am333n <133481818+am333n@users.noreply.github.com>
2025-05-07 04:31:05 +0000

    Deploying to gh-pages from @ am333n/my_portfolio@36df3c370029e31ca88d63bd428c05a340941c06 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3233162449"
+    serviceWorkerVersion: "3345342148"
   }
 });
